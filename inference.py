79b1a7f926e0bfc3f9eca521957644db543a4839
gan-of-culture <53971163+gan-of-culture@users.noreply.github.com>
2021-12-17 18:06:28 +0100

    add default scale 2
    added the parameter scale with a default value 2. So it might be easier to spot for non Python programmers.

--- inference.py
+++ inference.py
@@ -116,6 +116,6 @@ clip = core.ffms2.Source(source='input.webm')
 # convert colorspace + resizing
 clip = vs.core.resize.Bicubic(clip, width=848, height=480, format=vs.RGBS, matrix_in_s='709')
 # add scale param here to set the different scale. 2 and 4 are the possible values. 2 Is default
-clip = RealESRGAN(clip, fp16=False)
+clip = RealESRGAN(clip, scale=2, fp16=False)
 clip = vs.core.resize.Bicubic(clip, format=vs.YUV420P8, matrix_s="709")
 clip.set_output()
